37547b44244f81ad6d44c2f71cf31b414905d049
Dmitrii Krop <55913287+Suspend85@users.noreply.github.com>
2025-02-11 01:07:59 +0700

    Update install.sh
    menu refactoring

--- pipe/install.sh
+++ pipe/install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
+PROJ_NAME= "PIPE Network POP"
 # Проверка наличия необходимых утилит, установка если отсутствуют
 if ! command -v figlet &> /dev/null; then
     sudo apt update && sudo apt install -y figlet
-fi
-
-if ! command -v whiptail &> /dev/null; then
-    sudo apt update && sudo apt install -y whiptail
 fi
 
 # Определяем цвета для удобства
@@ -22,20 +18,21 @@ NC="\e[0m"
 # Вывод логотипа с помощью figlet
 echo -e "${YELLOW}$(figlet -l -k -w 150 -f slant "BlockRockNodes" | while IFS= read -r line; do echo -e "${YELLOW}$line${NC}"; done)${NC}"
 echo ""
-sleep 3
+sleep 1
 
 # функция установки пакетов-зависимостей
 install_dependencies() {
-    echo -e "${GREEN}Устанавливаем необходимые пакеты...${NC}"
+    echo -e "${GREEN}Installing depencies (Устанавливаем необходимые пакеты)...${NC}"
     sudo apt update && sudo apt install -y iptables make gcc nano automake autoconf nvme-cli libssl-dev libleveldb-dev tar clang bsdmainutils ncdu
 }
 
 # Функция для установки ноды
 install_node() {
-    echo "========================================"
-    echo -e "${BLUE}Начинаем установку ноды...${NC}"
-    echo "========================================"
-    
+    echo "================================================="
+    echo -e "${BLUE}Starting install ${PROJ_NAME} node (Устанавливаем ноду)...${NC}"
+    echo "================================================="
+    echo ""
+    sleep 1
     # Обновление и установка зависимостей
     source <(wget -O- 'https://raw.githubusercontent.com/Suspend85/Nodes/refs/heads/master/_utils/serv-prepare.sh')
     install_dependencies
@@ -53,27 +50,28 @@ install_node() {
     # Создание новой сессии в screen
     screen -S pipepop -dm
 
-    echo -e "${YELLOW}Введите ваш публичный адрес Solana:${NC}"
+    echo -e "${YELLOW}Enter your public Solana address (Введите свой Solana адрес):${NC}"
     read SOLANA_PUB_KEY
     
     # Запрос значения для RAM
-    echo -e "${YELLOW}Введите количество RAM в ГБ (целое число):${NC}"
+    echo -e "${YELLOW}Enter the amount of RAM, in GB (Integer) (Введите объем оперативки, в GB. Целое число):${NC}"
     read RAM
     
     # Запрос значения для max-disk
-    echo -e "${YELLOW}Введите количество max-disk в ГБ (целое число):${NC}"
+    echo -e "${YELLOW}Enter the amount of max-disk, in GB (Integer) (Введите размер диска, в GB. Целое число):${NC}"
     read DISK
     
     # Запуск команды с параметрами, с указанием публичного ключа Solana, RAM и max-disk
     screen -S pipepop -X stuff "./pop --ram $RAM --max-disk $DISK --cache-dir ~/pipe/download_cache --pubKey $SOLANA_PUB_KEY\n"
     sleep 2
-    # screen -S pipepop -X stuff "e4313e9d866ee3df\n"
-    echo -e "${GREEN}Процесс установки и запуска завершён!${NC}"
+    screen -S pipepop -X stuff "e4313e9d866ee3df\n"
+    echo -e "${GREEN}The installation and launch process is complete! (Установка и запуск завершен!)${NC}"
+    echo ""
 }
 
 # Функция для проверки статуса ноды
 check_status() {
-    echo -e "${BLUE}Проверка статуса ноды...${NC}"   
+    echo -e "${BLUE}Checking node status (Проверка статуса ноды)...${NC}"   
     cd pipe
     ./pop --status
     cd ..
@@ -81,7 +79,7 @@ check_status() {
 
 # Функция для проверки поинтов ноды
 check_points() {
-    echo -e "${BLUE}Проверка поинтов ноды...${NC}"
+    echo -e "${BLUE}Checking node points (Проверка поинтов ноды)...${NC}"
     cd pipe
     ./pop --points-route
     cd ..
@@ -89,28 +87,28 @@ check_points() {
 
 # Функция для создания реф-кода
 generate_referral() {
-    echo -e "${BLUE}Генерация реферрального кода...${NC}"
+    echo -e "${BLUE}Generating referral code (Создание реферрального кода)...${NC}"
     cd pipe
     ./pop --gen-referral-route
     cd ..
 }
 
 update_node() {
-    echo -e "${BLUE}Обновление до версии 0.2.4...${NC}"
+    echo -e "${BLUE}Update version to 0.2.4 (Обновление до версии 0.2.4) ...${NC}"
 
     # Остановка процесса pop
-    echo -e "${YELLOW}Останавливаем службу pipe-pop...${NC}"
+    echo -e "${YELLOW}Stopping service pipe-pop (Останавливаем службу pipe-pop)...${NC}"
     ps aux | grep '[p]op' | awk '{print $2}' | xargs kill
 
     # Переход в директорию pipe
     cd ~/pipe
 
     # Удаление старой версии pop
-    echo -e "${YELLOW}Удаляем старую версию pop...${NC}"
+    echo -e "${YELLOW}Deleting old version of pop (Удаляем старую версию pop)...${NC}"
     rm -f pop
 
     # Скачивание новой версии pop
-    echo -e "${YELLOW}Скачиваем новую версию pop...${NC}"
+    echo -e "${YELLOW}Downloading new version of pop (Скачиваем новую версию pop)...${NC}"
     wget -O pop "https://dl.pipecdn.app/v0.2.4/pop"
 
     # Делаем файл исполнимым
@@ -130,12 +128,12 @@ update_node() {
     sleep 5
     screen -S pipepop -X stuff "y\n"
     
-    echo -e "${GREEN}Обновление завершено!${NC}"
+    echo -e "${GREEN}Update complete! (Обновление завершено!).${NC}"
 }
 
 # Функция для удаления ноды
 remove_node() {
-    echo -e "${BLUE}Удаляем ноду...${NC}"
+    echo -e "${BLUE}Deleting the ${PROJ_NAME} node (Удаляем ноду)...${NC}"
     pkill -f pop
 
     # Завершаем сеанс screen с именем 'pipepop' и удаляем его
@@ -144,28 +142,35 @@ remove_node() {
     # Удаление файлов ноды
     sudo rm -rf ~/pipe
 
-    echo -e "${GREEN}Нода успешно удалена!${NC}"
+    echo -e "${GREEN}The ${PROJ_NAME} node has been successfully removed! (Нода успешно удалена!).${NC}"
 }
 
 # Основное меню
-CHOICE=$(whiptail --title "Меню действий" \
-    --menu "Выберите действие:" 18 50 7 \
-    "1" "Установка ноды" \
-    "2" "Проверка статуса ноды" \
-    "3" "Проверка поинтов ноды" \
-    "4" "Создание реф-кода" \
-    "5" "Удаление ноды" \
-    "6" "Обновление ноды" \
-    "7" "Выход" \
-    3>&1 1>&2 2>&3)
-
-case $CHOICE in
+while true; do
+  echo ""
+  echo -e "${YELLOW}"
+  echo -e "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  echo -e "${GREEN} ${PROJ_NAME} - Node Menu (Меню):"
+  echo -e "${YELLOW}%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  echo "1. Install Node (Установить ноду)"
+  echo "2. Check Node logs (Проверить статус ноды)"
+  echo "3. Chech Node points (Проверить поинты ноды)"
+  echo "4. Generate feferral code (Создать реф. код)"
+  echo "5. Delete Node (Удалить ноду)"
+  echo "6. Update Node (Обновить ноду)"
+  echo "7. Exit (Выход)"
+  echo -e "${YELLOW}"
+  read -p "Choose an option (Выберите пункт): " choice
+  echo -e "${NO_COLOR}"
+  
+  case $choice in
     1) install_node ;;
     2) check_status ;;
     3) check_points ;;
     4) generate_referral ;;
     5) remove_node ;;
     6) update_node ;;
-    7) echo -e "${CYAN}Выход из программы.${NC}" ;;
-    *) echo -e "${RED}Неверный выбор. Завершение программы.${NC}" ;;
-esac
+    7) break ;;
+    *) echo -e "${RED}Invalid option. Try again (Некорректный выбор. Попробуйте еще).${NC}" ;;
+  esac
+done
