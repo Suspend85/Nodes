d43141bd263af2197db73e8d2844086883613ab7
Dmitrii Krop <55913287+Suspend85@users.noreply.github.com>
2025-03-19 05:42:02 +0700

    Update validator_update.sh
    fixes

--- ZeroGravity/validator_update.sh
+++ ZeroGravity/validator_update.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 PROJ_NAME="ZeroGravity (0,G)"
-# Проверка наличия необходимых утилит, установка если отсутствуют
-if ! command -v figlet &> /dev/null; then
-    sudo apt update && sudo apt install -y figlet
-fi
-
-# Определяем цвета для удобства
-YELLOW="\e[33m"
-CYAN="\e[36m"
-BLUE="\e[34m"
-GREEN="\e[32m"
-MAGENTA='\033[1;35m'
-RED="\e[31m"
-PINK="\e[35m"
-NC="\e[0m"
-
-# Вывод логотипа с помощью figlet
-echo -e "${YELLOW}$(figlet -l -k -w 150 -f slant "BlockRockNodes" | while IFS= read -r line; do echo -e "${YELLOW}$line${NC}"; done)${NC}"
-echo ""
-sleep 1
 
 echo ''
 echo -e "\e[1m\e[32m========================================================================"
