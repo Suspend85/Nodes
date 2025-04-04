e64b78186148e8f7c6e0cd94031f24f27aa62223
Dmitrii Krop <55913287+Suspend85@users.noreply.github.com>
2025-04-04 17:12:21 +0700

    Update install.sh

--- Hyperbolic/install.sh
+++ Hyperbolic/install.sh
@@ -21,16 +21,6 @@ echo -e "${YELLOW}$(figlet -l -k -w 150 -f slant "BlockRockNodes" | while IFS= r
 echo ""
 sleep 1
 
-
-# Цвета текста
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # Нет цвета (сброс цвета)
-
 # Проверка наличия curl и установка, если не установлен
 if ! command -v curl &> /dev/null; then
     sudo apt update
